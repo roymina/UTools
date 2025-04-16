ffa83454bd6959079d9c842f33f0e0ecd0c98107
vip <371162760@qq.com>
2025-04-16 14:05:28 +0800

    新增[ShowIf]标签

--- Assets/UTools/Example/UFindTests/_TestUFind.cs
+++ Assets/UTools/Example/UFindTests/_TestUFind.cs
@@ -9,11 +9,19 @@ public class _TestUFind : UBehaviour
     [Child("customName")] TextMeshProUGUI childC;
     [Child] Image imgUFind;
     [Resource] Sprite ufind;
+    public bool UseDragGet = false;
+    [ShowIf("UseDragGet")]
+    public GameObject MyGameObject;
     void Start()
     {
         childA.color = UnityEngine.Color.cyan;
         childB.color = UnityEngine.Color.cyan;
         childC.color = UnityEngine.Color.cyan;
         imgUFind.sprite = ufind;
+        if (!UseDragGet)
+        {
+            MyGameObject = gameObject;
+        }
+        Debug.Log(MyGameObject);
     }
 }
