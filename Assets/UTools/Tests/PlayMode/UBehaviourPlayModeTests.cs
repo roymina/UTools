6373f1a8c90dbdba39d756e23855001fb60baf82
Roy <jnmcluo@163.com>
2026-04-10 16:12:16 +0800

    feat: add Children attribute support to UFind

--- Assets/UTools/Tests/PlayMode/UBehaviourPlayModeTests.cs
+++ Assets/UTools/Tests/PlayMode/UBehaviourPlayModeTests.cs
@@ -1,6 +1,9 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.TestTools;
 
 namespace UTools.Tests
@@ -27,6 +30,77 @@ namespace UTools.Tests
             Object.Destroy(root);
         }
 
+        [UnityTest]
+        public IEnumerator UBehaviour_AutoBindsChildrenCollections()
+        {
+            GameObject root = CreateChildrenRoot();
+            ChildrenCollectionBehaviour behaviour = root.AddComponent<ChildrenCollectionBehaviour>();
+
+            yield return null;
+
+            Assert.That(behaviour.NamedParent, Is.Not.Null);
+            Assert.That(behaviour.NamedChildren, Has.Count.EqualTo(3));
+            Assert.That(behaviour.NamedChildren, Has.All.Not.Null);
+            Assert.That(behaviour.NamedChildren[0].name, Is.EqualTo("ActiveChild"));
+            Assert.That(behaviour.NamedChildren[1].name, Is.EqualTo("InactiveChild"));
+            Assert.That(behaviour.NamedChildren[2].name, Is.EqualTo("NestedParent"));
+
+            Assert.That(behaviour.NamedDescendents, Has.Count.EqualTo(4));
+            Assert.That(behaviour.NamedDescendents.Exists(child => child.name == "Grandchild"), Is.True);
+
+            Assert.That(behaviour.ActiveOnlyChildren, Has.Count.EqualTo(2));
+            Assert.That(behaviour.ActiveOnlyChildren.Exists(child => child.name == "InactiveChild"), Is.False);
+
+            Assert.That(behaviour.NamedChildColliders, Has.Count.EqualTo(2));
+            Assert.That(behaviour.NamedChildColliders[0].gameObject.name, Is.EqualTo("ActiveChild"));
+            Assert.That(behaviour.NamedChildColliders[1].gameObject.name, Is.EqualTo("InactiveChild"));
+
+            Object.Destroy(root);
+        }
+
+        [UnityTest]
+        public IEnumerator UBehaviour_LogsErrorsForMissingParentAndInvalidChildrenList()
+        {
+            LogAssert.Expect(LogType.Error, new Regex("GameObject 'MissingParent' not found under Root", RegexOptions.IgnoreCase));
+            LogAssert.Expect(LogType.Error, new Regex("ChildrenAttribute on InvalidValues must target List<GameObject> or List<Component>", RegexOptions.IgnoreCase));
+
+            GameObject root = CreateChildrenRoot();
+            InvalidChildrenCollectionBehaviour behaviour = root.AddComponent<InvalidChildrenCollectionBehaviour>();
+
+            yield return null;
+
+            Assert.That(behaviour.MissingParent, Is.Null);
+            Assert.That(behaviour.InvalidValues, Is.Null);
+
+            Object.Destroy(root);
+        }
+
+        private static GameObject CreateChildrenRoot()
+        {
+            GameObject root = new("Root");
+
+            GameObject namedParent = new("NamedParent");
+            namedParent.transform.SetParent(root.transform);
+
+            GameObject activeChild = new("ActiveChild");
+            activeChild.transform.SetParent(namedParent.transform);
+            activeChild.AddComponent<BoxCollider>();
+
+            GameObject inactiveChild = new("InactiveChild");
+            inactiveChild.transform.SetParent(namedParent.transform);
+            inactiveChild.AddComponent<BoxCollider>();
+            inactiveChild.SetActive(false);
+
+            GameObject nestedParent = new("NestedParent");
+            nestedParent.transform.SetParent(namedParent.transform);
+
+            GameObject grandchild = new("Grandchild");
+            grandchild.transform.SetParent(nestedParent.transform);
+            grandchild.AddComponent<BoxCollider>();
+
+            return root;
+        }
+
         private sealed class SampleBehaviour : UBehaviour
         {
             [Comp]
@@ -34,6 +108,33 @@ namespace UTools.Tests
 
             [Child("Target")]
             public GameObject Target;
+        }
+
+        private sealed class ChildrenCollectionBehaviour : UBehaviour
+        {
+            [Children]
+            public List<GameObject> NamedParent;
+
+            [Children("NamedParent")]
+            public List<GameObject> NamedChildren;
+
+            [Children("NamedParent", includeDecendents = true)]
+            public List<GameObject> NamedDescendents;
+
+            [Children("NamedParent", includeInactive = false)]
+            public List<GameObject> ActiveOnlyChildren;
+
+            [Children("NamedParent")]
+            public List<BoxCollider> NamedChildColliders;
+        }
+
+        private sealed class InvalidChildrenCollectionBehaviour : UBehaviour
+        {
+            [Children("MissingParent")]
+            public List<GameObject> MissingParent;
+
+            [Children("NamedParent")]
+            public List<int> InvalidValues;
         }
     }
 }
